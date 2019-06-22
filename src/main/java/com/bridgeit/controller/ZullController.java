d5fee794f69552cf088306bea4b3f7f8bc417aa1
MobiComp <mahakbansal02@gmail.com>
2019-06-22 15:21:38 +0530

    Zull-Apt gateway commit

---
+++ src/main/java/com/bridgeit/controller/ZullController.java
@@ -0,0 +1,34 @@
+package com.bridgeit.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Controller;
+
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+@Controller
+@Primary
+@EnableAutoConfiguration
+public class ZullController implements SwaggerResourcesProvider  {
+
+	@Override
+	public List<SwaggerResource> get() {
+		 List<SwaggerResource> resources = new ArrayList<>();
+	        resources.add(swaggerResource("userservice", "/userservice/v2/api-docs", "2.0"));
+	        resources.add(swaggerResource("noteservice", "/noteservice/v2/api-docs", "2.0"));
+	        return resources;
+	}
+	
+	 private SwaggerResource swaggerResource(String name, String location, String version) {
+	        SwaggerResource swaggerResource = new SwaggerResource();
+	        swaggerResource.setName(name);
+	        swaggerResource.setLocation(location);
+	        swaggerResource.setSwaggerVersion(version);
+	        return swaggerResource;
+	    }
+
+}
+
